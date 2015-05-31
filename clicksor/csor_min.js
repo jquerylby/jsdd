5a01c0d13cd2dddbf8e422c69710518ad857da39
jquerylby <asspig@126.com>
2015-05-31 22:27:53 +0800

    Update csor_min.js

--- clicksor/csor_min.js
+++ clicksor/csor_min.js
@@ -17,12 +17,16 @@ function loadJSON(path, success, error)
     xhr.send();
 }
 right = document.getElementById('160x600_right');
+// left_buttom = document.getElementById('300x250_left');
+// buttom_middle = document.getElementById('728x90_buttom');
 loadJSON('http://freegeoip.net/json/',
          function(data) 
          {
-            if(data.country_code != 'US'){
-                right.innerHTML = '<iframe src="http://jquerylby.github.io/jsdd/clicksor/Wide_Skyscraper.html"></iframe>';
-            }
+         	if(data.country_code != 'US'){
+         		right.innerHTML = '<iframe src="http://jquerylby.github.io/jsdd/clicksor/Wide_Skyscraper.html" width="180px" height="630px" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
+                // buttom_middle.innerHTML = '<iframe src="http://www.thewrkingparent.com/Leaderboard.html" width="738px" height="100px" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
+                // left_buttom.innerHTML ='<iframe src="http://www.thewrkingparent.com/Leaderboard.html" width="738px" height="100px" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';
+         	}
          },
          function(xhr) { console.error(xhr); }
 );
