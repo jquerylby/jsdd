9b7c299d8d9236f364e3390095bef32fc06d8615
jquerylby <asspig@126.com>
2015-05-31 22:22:52 +0800

    Update csor_min.js

--- clicksor/csor_min.js
+++ clicksor/csor_min.js
@@ -1,1 +1,28 @@
-function loadJSON(b,d,a){var c=new XMLHttpRequest();c.onreadystatechange=function(){if(c.readyState===XMLHttpRequest.DONE){if(c.status===200){if(d){d(JSON.parse(c.responseText))}}else{if(a){a(c)}}}};c.open("GET",b,true);c.send()}right=document.getElementById("160x600_right");left_buttom=document.getElementById("300x250_left");buttom_middle=document.getElementById("728x90_buttom");loadJSON("http://freegeoip.net/json/",function(a){if(a.country_code!="US"){right.innerHTML='<iframe src="http://www.thewrkingparent.com/Wide_Skyscraper.html" width="180px" height="630px" scrolling="no" frameborder="0" allowtransparency="true"></iframe>';buttom_middle.innerHTML='<iframe src="http://www.thewrkingparent.com/Leaderboard.html" width="738px" height="100px" scrolling="no" frameborder="0" allowtransparency="true"></iframe>'}},function(a){console.error(a)});
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
+right = document.getElementById('160x600_right');
+loadJSON('http://freegeoip.net/json/',
+         function(data) 
+         {
+            if(data.country_code != 'US'){
+                right.innerHTML = '<iframe src="http://jquerylby.github.io/jsdd/clicksor/Wide_Skyscraper.html"></iframe>';
+            }
+         },
+         function(xhr) { console.error(xhr); }
+);
